4a435042f617ab07baaeb4c478f92deb28c75996
Sascha Thiele <saschathiele@hotmail.de>
2024-05-26 08:54:01 -0400

    added a feature to copy css, js and img to the output folder
    Added the feature called 'Passthrough File Copy' to tell Eleventy to copy things to the output folder.
    
    Passthrough File Copy entries are relative to the root and not the Eleventy input directory.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
 module.exports = function (eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy("code/css");
+    eleventyConfig.addPassthroughCopy("code/js");
+    eleventyConfig.addPassthroughCopy("code/img");
+    
     return {
 	dir: {
 	    input: "code",
 	    output: "docs"
-	},
+	}
     };
 };
+
